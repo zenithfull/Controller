98130aaa4e3b0fa249d73c797cfaad43eae8f5c8
zenith <zenithfull@gmail.com>
2020-06-29 21:49:12 +0900

    モーションコントロール処理を追加

--- app/src/main/java/com/example/controller/MainActivity.java
+++ app/src/main/java/com/example/controller/MainActivity.java
@@ -15,58 +15,139 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager sensorManager;
     private TextView textView, textInfo;
 
+    protected final static double RAD2DEG = 180/Math.PI;
+
+    float[] rotationMatrix = new float[9];
+    float[] gravity = new float[3];
+    float[] geomagnetic = new float[3];
+    float[] attitude = new float[3];
+
+
+
+    private int startupX , startupY, startupZ;
+    private int sensorX, sensorY, sensorZ;
+
+    private String direction, leftAndRight;
+
+    private static final float initializePos = -9999999;
+
+    private static final float forwardActionVal = 20;
+    private static final float backActionVal = -20;
+    private static final float leftActionVal = -20;
+    private static final float rightActionVal = 20;
+
+    MqttService mqttService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
         textInfo = findViewById(R.id.text_info);
 
         textView = findViewById(R.id.text_view);
+
+//        mqttService = new MqttService();
+
+        initSensor();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        Sensor accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(
+                this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(
+                this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+                SensorManager.SENSOR_DELAY_GAME);
 
-        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
+//        mqttService.connect();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
+//        mqttService.disconnect();
+
         sensorManager.unregisterListener(this);
     }
 
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        float sensorX , sensorY, sensorZ;
+    public void onSensorChanged(SensorEvent event) {
 
-        if(sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            sensorX = sensorEvent.values[0];
-            sensorY = sensorEvent.values[1];
-            sensorZ = sensorEvent.values[2];
+        switch(event.sensor.getType()){
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                geomagnetic = event.values.clone();
+                break;
+            case Sensor.TYPE_ACCELEROMETER:
+                gravity = event.values.clone();
+                break;
+        }
 
-            String strTmp = "加速度センサー\n"
+        if(geomagnetic != null && gravity != null) {
+
+            SensorManager.getRotationMatrix(
+                    rotationMatrix, null,
+                    gravity, geomagnetic);
+
+            SensorManager.getOrientation(
+                    rotationMatrix,
+                    attitude);
+
+            sensorX = (int)(attitude[0] * RAD2DEG);
+            sensorY = (int)(attitude[1] * RAD2DEG);
+            sensorZ = (int)(attitude[2] * RAD2DEG);
+
+            if (startupX == initializePos) {
+                startupX = sensorX;
+                startupY = sensorY;
+                startupZ = sensorZ;
+            }
+
+            if (sensorY > startupY + forwardActionVal) {
+                direction = "forward";
+            } else if (sensorY < startupY + backActionVal) {
+                direction = "back";
+            } else {
+                direction = "stop";
+            }
+
+            if (sensorZ < startupZ + leftActionVal) {
+                leftAndRight = "left";
+            } else if (sensorZ > startupZ + rightActionVal) {
+                leftAndRight = "right";
+            } else {
+                leftAndRight = "straight";
+            }
+
+            String strTmp = "端末角度\n"
                     + " X: " + sensorX + "\n"
                     + " Y: " + sensorY + "\n"
-                    + " Z: " + sensorZ;
+                    + " Z: " + sensorZ + "\n"
+                    + " direction: " + direction + "\n"
+                    + " left / right: " + leftAndRight + "\n";
 
             textView.setText(strTmp);
 
-            showInfo(sensorEvent);
+//            showInfo(event);
 
         }
+
+
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    protected void initSensor(){
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
     }
 
     private void showInfo(SensorEvent sensorEvent) {
--- app/src/main/java/com/example/controller/MqttService.java
+++ app/src/main/java/com/example/controller/MqttService.java
@@ -1,19 +1,92 @@
 package com.example.controller;
 
-import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttClientStatusCallback;
 import com.amazonaws.mobileconnectors.iot.AWSIotMqttManager;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttNewMessageCallback;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttQos;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.iot.AWSIotClient;
 
-public class MqttService extends Service {
-    private AWSIotMqttManager mpttManager;
+import java.io.UnsupportedEncodingException;
+import java.security.KeyStore;
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+public class MqttService {
+    static final String LOG_TAG = MqttService.class.getCanonicalName();
+
+    // IoT endpoint
+    // AWS Iot CLI describe-endpoint call returns: XXXXXXXXXX.iot.<region>.amazonaws.com
+    private static final String CUSTOMER_SPECIFIC_ENDPOINT = "CHANGE_ME";
+    // Name of the AWS IoT policy to attach to a newly created certificate
+    private static final String AWS_IOT_POLICY_NAME = "CHANGE_ME";
+
+    // Region of AWS IoT
+    private static final Regions MY_REGION = Regions.US_EAST_1;
+    // Filename of KeyStore file on the filesystem
+    private static final String KEYSTORE_NAME = "iot_keystore";
+    // Password for the private key in the KeyStore
+    private static final String KEYSTORE_PASSWORD = "password";
+    // Certificate and key aliases in the KeyStore
+    private static final String CERTIFICATE_ID = "default";
+
+    AWSIotClient mIotAndroidClient;
+    AWSIotMqttManager mqttManager;
+
+    KeyStore clientKeyStore = null;
+    String certificateId;
+
+    public void connect() {
+
+        try {
+            mqttManager.connect(clientKeyStore, new AWSIotMqttClientStatusCallback() {
+                @Override
+                public void onStatusChanged(final AWSIotMqttClientStatus status,
+                                            final Throwable throwable) {
+                    Log.d(LOG_TAG, "Status = " + String.valueOf(status));
+
+                }
+            });
+        } catch (final Exception e) {
+            Log.e(LOG_TAG, "Connection error.", e);
+        }
     }
+
+    public void subscribe(String topic) {
+
+        try {
+            mqttManager.subscribeToTopic(topic, AWSIotMqttQos.QOS0,
+                    new AWSIotMqttNewMessageCallback() {
+                        @Override
+                        public void onMessageArrived(final String topic, final byte[] data) {
+                        }
+                    });
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Subscription error.", e);
+        }
+    }
+
+    public void publish(String topic, String msg) {
+
+        try {
+            mqttManager.publishString(msg, topic, AWSIotMqttQos.QOS0);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Publish error.", e);
+        }
+    }
+
+
+    public void disconnect() {
+        try {
+            mqttManager.disconnect();
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Disconnect error.", e);
+        }
+    }
+
 }
